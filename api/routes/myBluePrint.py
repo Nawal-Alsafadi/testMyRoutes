b21e55d6920e912a91a98ea4eaf2a9253d7bb416
Nawal-Alsafadi <155569547+Nawal-Alsafadi@users.noreply.github.com>
2024-01-04 16:54:42 +0300

    Update myBluePrint.py

--- api/routes/myBluePrint.py
+++ api/routes/myBluePrint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
-from models.myModels import getString
-from models.myModels import getString2
+from api.models.myModels import getString
+from api.models.myModels import getString2
 
 
 
